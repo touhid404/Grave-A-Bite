6be5bae38b90c687f1bab6ec986fee16da8ddbe5
touhid404 <touhidulislamriyadh@gmail.com>
2026-01-31 00:36:40 +0600

    deploy issues fixed

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  // baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  // baseURL: process.env.NEXT_PUBLIC_CLIENT_URL,
 });
